dfe519f70a55261ff7ee761c10192fe5d68c6b9b
Italo Ferreira <grd788@gmail.com>
2021-07-27 20:35:26 -0300

    database docker

--- src/shared/infra/http/typeorm/index.ts
+++ src/shared/infra/http/typeorm/index.ts
@@ -1,12 +1,12 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = "database_ignite"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOpitions = await getConnectionOptions();
 
 
   return createConnection(
     Object.assign(defaultOpitions,{
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+
       database: process.env.NODE_ENV === "test" ? "rentx_test" : defaultOpitions.database
     })
   )
